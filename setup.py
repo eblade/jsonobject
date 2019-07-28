289691f98b771de1448ae5bbca138cdfa2c0cbf4
Johan Egneblad <johan@egneblad.se>
2019-07-28 21:53:34 +0200

    Tidy up

--- setup.py
+++ setup.py
@@ -4,18 +4,22 @@
 from setuptools import setup
 
 name_ = 'lindh-jsonobject'
+github_name = 'jsonobject'
 version_ = '1.3.3'
 packages_ = [
     'lindh.jsonobject',
 ]
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
 classifiers = [
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
     "Development Status :: 5 - Production/Stable",
     "Intended Audience :: Developers",
     "License :: OSI Approved :: MIT License",
-    "Operating System :: POSIX",
-    "Operating System :: POSIX :: Linux",
-    "Programming Language :: Python :: 3",
 ]
 
 setup(
@@ -24,10 +28,12 @@ setup(
     author='Johan Egneblad',
     author_email='johan@DELETEMEegneblad.se',
     description='JSON serializable objects',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     license="MIT",
-    url='https://github.com/eblade/'+name_,
+    url='https://github.com/eblade/'+github_name,
     download_url=('https://github.com/eblade/%s/archive/v%s.tar.gz'
-                  % (name_, version_)),
+                  % (github_name, version_)),
     packages=packages_,
     install_requires=[],
     classifiers=classifiers,
