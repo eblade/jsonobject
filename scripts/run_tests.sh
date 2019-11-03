5de2e8756c49decb98a6289993a010adf0a97230
Johan Egneblad <johan@egneblad.se>
2019-11-03 20:16:31 +0100

    Add coverage check

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,7 +2,7 @@
 # Usage
 #   $ ./scripts/run_tests.sh
 # or
-#   $ ./scripts/run_tests.sh --cov pycvodes --cov-report html
+#   $ ./scripts/run_tests.sh --cov lindh --cov-report html
 #python -m pytest --doctest-modules --pep8 --flakes $@
 python3 -m pytest --pep8 --flakes -v $@
 python3 -m doctest README.rst
