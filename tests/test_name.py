c1860e62a8f8de9520aa6769f1942c814fedce2e
Johan Egneblad <johan@egneblad.se>
2019-11-05 22:03:04 +0100

    Add name duplication tests for subclasses

--- tests/test_name.py
+++ tests/test_name.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import pytest
 from lindh.jsonobject import PropertySet, Property
 
 
@@ -29,3 +30,24 @@ def test_getting_property_from_class_object_should_return_property_object():
     po = O.id
     assert isinstance(po, Property)
     assert po.serialized_name == 'id'
+
+
+def test_same_name_in_subclass_should_raise_error():
+    class Base(PropertySet):
+        a: int = Property()
+
+    with pytest.raises(AttributeError):
+        class Sub(Base):
+            a: str = Property()
+
+
+def test_same_name_in_two_baseclasses_should_raise_error():
+    class Base1(PropertySet):
+        a: int = Property()
+
+    class Base2(PropertySet):
+        a: str = Property()
+
+    with pytest.raises(AttributeError):
+        class Sub(Base1, Base2):
+            pass
