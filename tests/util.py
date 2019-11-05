3906177d418835b2a9d1494acb41293bc23280b5
Johan Egneblad <johan@egneblad.se>
2019-11-05 22:35:20 +0100

    Better reason

--- tests/util.py
+++ tests/util.py
@@ -2,4 +2,4 @@ import sys
 import pytest
 
 
-uses_typehints = pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.6 or higher")
+uses_typehints = pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
