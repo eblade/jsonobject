43f1cb412343b98362f9c6e420993009329f1c9d
Johan Egneblad <johan@egneblad.se>
2023-09-25 16:21:20 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -12,6 +12,5 @@ if [ -d lindh_jsonobjecy.egg-info ]; then
     rm -rf lindh_jsonobjecy.egg-info
 fi
 
-pipenv update --dev setuptools wheel twine
-pipenv run python setup.py sdist bdist_wheel
-pipenv run python -m twine upload dist/*
+python -m build
+#python -m twine upload dist/*
